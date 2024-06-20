d729b0e75a2e77f9075f4c258906263a15b75050
LAPTOP-TA6FH0UP\Michael <mikeg@surgepay.app>
2024-06-20 09:09:40 +0800

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/ui/MainScreen.kt
@@ -0,0 +1,16 @@
+package ui
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+
+}
+
+
+@Composable
+@Preview
+private fun MainScreenPreview() {
+
+}
